2a6e8b5bcc22e59e930a90c2f4525912d1029e85
Diego Salazar <diego.salazarc4@gmail.com>
2025-04-18 00:12:58 -0500

    Initial configuration of TripExpense project

---
+++ TripExpense/src/main/java/com/tripexpense/TripExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.tripexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripExpenseApplication.class, args);
+	}
+
+}
---
+++ TripExpense/src/test/java/com/tripexpense/TripExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
