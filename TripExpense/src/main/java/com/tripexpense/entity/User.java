d33a463d1007d9c575c6722d1c60ea7db85c2658
Diego Salazar <diego.salazarc4@gmail.com>
2025-04-22 00:08:29 -0500

    Create user entity, service, DTO, repository and controller

---
+++ TripExpense/src/main/java/com/tripexpense/entity/User.java
@@ -0,0 +1,132 @@
+package com.tripexpense.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = false)
+    @Email(message = "El email no es valido")
+    @NotBlank(message = "El email es obligatorio")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 7, message = "La contraseña debe tener al menos 7 caracteres")
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "El nombre es obligatorio")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "El apellido es obligatorio")
+    private String lastName;
+
+    @Column(unique = true)
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$",
+            message = "El teléfono debe tener entre 10 y 15 dígitos")
+    private String phone;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    public User() {
+    }
+
+    public User(Long id, String email, String password, String firstName, String lastName, String phone, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ TripExpense/src/main/java/com/tripexpense/service/impl/UserServiceImpl.java
@@ -0,0 +1,93 @@
+package com.tripexpense.service.impl;
+
+import com.tripexpense.dto.UserDTO;
+import com.tripexpense.entity.User;
+import com.tripexpense.repository.UserRepository;
+import com.tripexpense.service.interfac.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDTO createUser(UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.getEmail())){
+            throw new RuntimeException("El email ya se encuentra registrado");
+        }
+        User user = new User();
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setPassword(userDTO.getPassword());
+
+
+        return convertToDTO(userRepository.save(user));
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDTO getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()){
+            return convertToDTO(user.get());
+        } else {
+            throw new RuntimeException("Usuario no encontrado");
+        }
+    }
+
+    @Override
+    public UserDTO updateUser(Long id, UserDTO userDTO) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()){
+            User user = userOptional.get();
+            user.setFirstName(userDTO.getFirstName());
+            user.setLastName(userDTO.getLastName());
+            user.setEmail(userDTO.getEmail());
+            user.setPassword(userDTO.getPassword());
+            user.setPassword(userDTO.getPassword());
+
+            return convertToDTO(userRepository.save(user));
+        } else {
+            throw new RuntimeException("Usuario no encontrado");
+        }
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+        if (userRepository.existsById(id)){
+            userRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Usuario no encontrado");
+        }
+    }
+
+    private UserDTO convertToDTO(User user){
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setPhone(user.getPhone());
+
+        return userDTO;
+    }
+}
